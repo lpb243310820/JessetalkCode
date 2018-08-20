696a8a2615d0fecbe281b588e9b29e2ade726e6b
李朋勃 <243310820@qq.com>
2018-08-20 14:27:16 +0800

    ASP.NET Core快速入门 代码完结

---
+++ MvcCookieAuthSample2/Data/WebHostMigrationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MvcCookieAuthSample2.Data
+{
+    public static class WebHostMigrationExtensions
+    {
+        public static IWebHost MigrateDbContext<TContext>(this IWebHost host, Action<TContext, IServiceProvider> sedder)
+            where TContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    context.Database.Migrate();
+                    sedder(context, services);
+
+                    logger.LogInformation($"执行DBcontext {typeof(TContext).Name} seed 方法执行成功");
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, $"执行DBcontext {typeof(TContext).Name} seed 方法失败");
+                }
+            }
+
+            return host;
+        }
+    }
+}
---
+++ MvcCookieAuthSample2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MvcCookieAuthSample2.Data;
+
+namespace MvcCookieAuthSample2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).MigrateDbContext<ApplicationDbContext>((context, services) =>
+            {
+                new ApplicationDbContextSeed().SeedAsync(context, services).Wait();
+            }).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
